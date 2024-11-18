ce3d6134c9427942ab8ba0ea2ec685de8589396a
Marycielo29 <marycielomb29@gmail.com>
2024-11-18 11:59:55 -0500

    Update config1.php

--- config/config1.php
+++ config/config1.php
@@ -1,11 +1,10 @@
 <?php
 
 const BD_HOST = 'localhost';
-const BD_NAME = '';
 const BD_USER = '';
 const BD_PASSWORD = '';
-
-const BD_CHARSET = 'utf-8';
+const BD_NAME = '';
+const BD_CHARSET = 'utf8';
 // ingresar link del proyecto
 const BASE_URL = '';
 
